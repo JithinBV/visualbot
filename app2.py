36f87381f84a258a5781e589bb20a61fc4f5140d
JithinBV <141017950+JithinBV@users.noreply.github.com>
2023-10-25 16:50:06 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,7 +1,6 @@
 import streamlit as  st
-from langchain.agents import create_csv_agent
+from langchain.agents import create_pandas_dataframe_agent
 from langchain.llms import AzureOpenAI
-import pandas as pd
 
 import os
 from dotenv import load_dotenv
@@ -26,21 +25,22 @@ def main():
     user_csv = st.file_uploader("upload your csv file", type = 'csv', accept_multiple_files=True)
     
     if user_csv is not None:
-         
+       
         
-        
+    
         user_question= st.text_input("ASK YOUR QUESTION:")
         
         
         llm = AzureOpenAI(deployment_name=AZURE_OPENAI_NAME, temperature=0)
 
-        agent = create_csv_agent(llm, user_csv,verbose=True)
+        agent = create_pandas_dataframe_agent(llm, user_csv,verbose=True)
         
         if user_question is not None and user_question != "":
             response = agent.run(user_question)
             
             st.spinner("Generating response.....")
             st.write(response)
+            st.bar_chart(response)
 
 
 if __name__ == "__main__":
