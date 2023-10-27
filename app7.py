653bef2c1fc5bf43c5d16b368d71a4c0c40008bb
JithinBV <141017950+JithinBV@users.noreply.github.com>
2023-10-27 10:45:07 +0530

    Create app7.py

---
+++ app7.py
@@ -0,0 +1,93 @@
+import streamlit as  st
+from langchain.agents import initialize_agent, Tool
+from langchain.agents import create_pandas_dataframe_agent
+from langchain.llms import AzureOpenAI
+import pandas as pd
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+os.environ["OPENAI_API_BASE"] = os.environ["AZURE_OPENAI_ENDPOINT"] = 'https://aoiaipsi.openai.azure.com/'
+
+os.environ["OPENAI_API_KEY"] = os.environ["AZURE_OPENAI_API_KEY"] = 'f769445c82844edda56668cb92806c21'
+
+os.environ["OPENAI_API_VERSION"] = os.environ["AZURE_OPENAI_API_VERSION"] = "2023-07-01-preview" #"2023-03-15-preview"
+
+os.environ["OPENAI_API_TYPE"] = "azure"
+
+AZURE_OPENAI_NAME = 'gpt-35-turbo-0301'
+
+
+
+
+
+
+def main():
+    
+    st.set_page_config(page_title="Ask your CSV")
+    st.header("Ask your CSV(agent)")
+    
+    user_csv = st.file_uploader("upload your csv file", type = 'csv', accept_multiple_files=True)
+    for f in user_csv:
+        st.write(f)
+        data_list = []
+        for f in user_csv:
+            data = pd.read_csv(f)
+            data_list.append(data)
+            df = pd.concat(data_list)
+    
+    if df is not None:
+        
+       
+        
+    
+        user_question= st.text_input("ASK YOUR QUESTION:")
+        
+        
+        
+        llm = AzureOpenAI(deployment_name=AZURE_OPENAI_NAME, temperature=0)
+
+        agent = create_pandas_dataframe_agent(llm, df,verbose=True)
+        adversarial_prompt="""
+            For the following query, if it requires drawing a table, reply as follows:
+            {"table": {"columns": ["column1", "column2", ...], "data": [[value1, value2, ...], [value1, value2, ...], ...]}}
+
+            If the query requires creating a bar chart, reply as follows:
+            {"bar": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...]}}
+
+            If the query requires creating a line chart, reply as follows:
+            {"line": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...]}}
+
+            There can only be two types of chart, "bar" and "line".
+
+            If it is just asking a question that requires neither, reply as follows:
+            {"answer": "answer"}
+            Example:
+            {"answer": "The title with the highest rating is 'Gilead'"}
+
+            If you do not know the answer, reply as follows:
+            {"answer": "I do not know."}
+
+            Return all output as a string.
+
+            All strings in "columns" list and data list, should be in double quotes,
+
+            For example: {"columns": ["title", "ratings_count"], "data": [["Gilead", 361], ["Spider's Web", 5164]]}
+
+            Lets think step by step.
+
+            Below is the query.
+            Query: 
+            """
+        
+        if user_question is not None and user_question != "":
+            response = agent.run(adversarial_prompt)
+            
+            st.spinner("Generating response.....")
+            st.write(response)
+            
+
+
+if __name__ == "__main__":
+    main()
