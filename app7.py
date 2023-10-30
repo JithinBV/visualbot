71682c6d34e0799eca20ef7bcc43d305489a3a3b
JithinBV <141017950+JithinBV@users.noreply.github.com>
2023-10-30 10:07:46 +0530

    Update app7.py

--- app7.py
+++ app7.py
@@ -36,21 +36,7 @@ def main():
         1. If the query requires a table, format your answer like this:
            {"table": {"columns": ["column1", "column2", ...], "data": [[value1, value2, ...], [value1, value2, ...], ...]}}
  
-        2. For a bar chart, respond like this:
-           {"bar": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...]}}
- 
-        3. If a line chart is more appropriate, your reply should look like this:
-           {"line": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...]}}
- 
-        Note: We only accommodate two types of charts: "bar" and "line".
- 
-        4. For a plain question that doesn't need a chart or table, your response should be:
-           {"answer": "Your answer goes here"}
- 
-        For example:
-           {"answer": "The Product with the highest Orders is '15143Exfo'"}
- 
- 
+       
         Return all output as a string. Remember to encase all strings in the "columns" list and data list in double quotes.
         For example: {"columns": ["Products", "Orders"], "data": [["51993Masc", 191], ["49631Foun", 152]]}
  
